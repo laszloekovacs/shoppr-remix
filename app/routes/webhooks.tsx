5b7a2aea2591c0c55082d53e6d109c0926588e44
Laszlo E Kovacs <laszloekovacs@gmail.com>
2024-03-09 20:48:26 +0100

    added notes for signature validation

--- app/routes/webhooks.tsx
+++ app/routes/webhooks.tsx
@@ -2,10 +2,17 @@ import { ActionFunctionArgs, json } from '@remix-run/node'
 
 export const action = async ({ request }: ActionFunctionArgs) => {
 	const event = await request.json()
+	const sig = request.headers.get('stripe-signature')
+
+	console.log(sig)
+
+	// check if the event came from stripe
+	//let	event = stripe.webhooks.constructEvent(body, sig, endpointSecret)
 
 	switch (event.type) {
 		case 'payment_intent.succeeded':
 			console.log('PaymentIntent was successful', event.data.object)
+			// handlePaymentIntentSucceeded(event)
 			break
 
 		default:

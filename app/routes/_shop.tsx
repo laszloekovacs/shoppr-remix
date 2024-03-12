70f8536d69e59af86595db9d029ded6866e6f963
laszloekovacs <laszloekovacs@gmail.com>
2024-03-12 16:02:48 +0100

    moved register, login path

--- app/routes/_shop.tsx
+++ app/routes/_shop.tsx
@@ -3,21 +3,20 @@ import { Link, Outlet } from '@remix-run/react'
 export default function ShopLayout() {
 	return (
 		<main className='grid grid-rows-layout min-h-screen'>
-			<div className='p-4'>
-				<Link to='/'>
-					<h1>Shoppr</h1>
-				</Link>
+			<Link to='/' className='p-4'>
+				<h1>Shoppr</h1>
+			</Link>
+
+			<div className='px-4'>
+				<Outlet />
+			</div>
+
+			<footer className='bg-neutral-900 pb-12 p-6 text-neutral-200'>
 				<nav className='flex gap-2'>
 					<Link to='/dashboard'>Dashboard</Link>
 					<Link to='/'>Home</Link>
 					<Link to='/account/cart'>Checkout</Link>
 				</nav>
-			</div>
-			<div className='px-4'>
-				<Outlet />
-			</div>
-
-			<footer className='bg-neutral-700 pb-12 p-6 text-neutral-200'>
 				<p>&copy; {new Date().getFullYear()} Shoppr</p>
 			</footer>
 		</main>

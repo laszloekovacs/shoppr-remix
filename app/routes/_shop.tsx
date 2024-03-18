6a90f11c2bece08c682a48be7116e86180e2e2fa
Laszlo E Kovacs <laszloekovacs@gmail.com>
2024-03-18 19:33:45 +0100

    Add QuickFilterBar component to ShopLayout

--- app/routes/_shop.tsx
+++ app/routes/_shop.tsx
@@ -1,19 +1,24 @@
 import { LoaderFunctionArgs } from '@remix-run/node'
 import { Link, Outlet, json, useLoaderData } from '@remix-run/react'
 import { Footer } from '~/components'
+import { db } from '~/services/database.server'
 import { auth } from '~/services/session.server'
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
 	const user = await auth.isAuthenticated(request)
-	return json({ user })
+
+	const departments = await db.products.distinct<string>('department')
+
+	return json({ user, departments })
 }
 
 export default function ShopLayout() {
-	const { user } = useLoaderData<typeof loader>()
+	const { user, departments } = useLoaderData<typeof loader>()
 
 	return (
 		<main className='grid grid-rows-layout min-h-screen p-4 lg:container lg:mx-auto'>
 			<ShopHeader user={user} />
+			<QuickFilterBar departments={departments} />
 			<Outlet />
 			<Footer />
 		</main>
@@ -35,5 +40,17 @@ const ShopHeader = ({ user }: { user: any }) => {
 				)}
 			</div>
 		</header>
+	)
+}
+
+const QuickFilterBar = ({ departments }: { departments: string[] }) => {
+	return (
+		<nav className='flex flex-row gap-8 text-lg font-bold'>
+			{departments.map(department => (
+				<Link key={department} to={`/`}>
+					{department}
+				</Link>
+			))}
+		</nav>
 	)
 }
--- app/routes/checkout.thankyou.tsx
+++ app/routes/checkout.thankyou.tsx
@@ -14,12 +14,30 @@ export default function CheckoutResultPage() {
 	const status = useLoaderData<typeof loader>()
 
 	return (
-		<div>
-			{status === 'success' && <h1>Checkout successful!</h1>}
-			{status === 'canceled' && <h1>Checkout canceled</h1>}
+		<section className='grid place-content-center p-8'>
+			<div className='flex flex-col gap-8 text-center'>
+				{status === 'success' ? <Success /> : <Cancelled />}
 
-			<Link to='/'>Home</Link>
-			<Link to='/account/cart'>Account</Link>
-		</div>
+				<div className='flex flex-row gap-8 justify-center'>
+					<Link to='/'>Return to Home</Link>
+					<Link to='/account/cart'>Back to Cart</Link>
+				</div>
+			</div>
+		</section>
 	)
 }
+
+const Success = () => (
+	<div>
+		<h1>Checkout successful!</h1>
+		<p>Thank you for your purchase</p>
+		<p>your order is on its way (not really)</p>
+	</div>
+)
+
+const Cancelled = () => (
+	<div>
+		<h1>Checkout canceled!</h1>
+		<p>Your cart is still waiting for you</p>
+	</div>
+)

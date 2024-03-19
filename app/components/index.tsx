c9c19ca1e3e797ed30b662b25f96c558ea1755d8
Laszlo E Kovacs <laszloekovacs@gmail.com>
2024-03-19 18:59:38 +0100

    removed styling

--- app/components/index.tsx
+++ app/components/index.tsx
@@ -1,9 +1,9 @@
 import { Link } from '@remix-run/react'
 
 export const Footer = () => (
-	<footer className='pb-12 p-6 border-t border-t-stone-500'>
-		<h3 className='text-lg mb-4 font-semibold'>Debug links</h3>
-		<nav className='flex gap-2'>
+	<footer>
+		<h3>Debug links</h3>
+		<nav className='d-flex gap-2'>
 			<Link to='/dashboard'>Dashboard</Link>
 			<Link to='/'>Home</Link>
 			<Link to='/account/cart'>Checkout</Link>
@@ -13,10 +13,10 @@ export const Footer = () => (
 
 export const Card = ({ title }: { title: string }) => {
 	return (
-		<div className='shadow-md rounded-md p-4 inline-block'>
-			<h3 className='text-lg font-semibold mb-2'>{title}</h3>
+		<div>
+			<h3>{title}</h3>
 			<img src='https://picsum.photos/200' />
-			<p className='text-sm text-stone-500'>This is a card component</p>
+			<span>This is a card component</span>
 		</div>
 	)
 }

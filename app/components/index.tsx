76f96379a091fd924aed2a8768349370f2631f6f
laszloekovacs <laszloekovacs@gmail.com>
2024-03-20 11:59:40 +0100

    Refactor footer and update styles

--- app/components/index.tsx
+++ app/components/index.tsx
@@ -1,13 +1,18 @@
 import { Link } from '@remix-run/react'
 
 export const Footer = () => (
-	<footer className='py-5'>
-		<h3>Debug links</h3>
-		<nav className='d-flex gap-2'>
-			<Link to='/dashboard'>Dashboard</Link>
-			<Link to='/'>Home</Link>
-			<Link to='/account/cart'>Cart</Link>
-		</nav>
+	<footer className='pt-4 pb-8 text-stone-500'>
+		<div className='flex flex-col md:flex-row gap-8 justify-evenly text-center'>
+			<nav className='flex gap-4 flex-col'>
+				<Link to='/dashboard'>Dashboard</Link>
+				<Link to='/'>Home</Link>
+				<Link to='/account/cart'>Cart</Link>
+			</nav>
+			<nav className='flex gap-4 flex-col'>
+				<a href='facebook.com'>Facebook</a>
+				<a href='github.com'>Github</a>
+			</nav>
+		</div>
 	</footer>
 )
 

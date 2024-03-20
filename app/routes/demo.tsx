4dda27dcb3e11412ba77f12610e9c7bb3546b38f
laszloekovacs <laszloekovacs@gmail.com>
2024-03-20 16:10:12 +0100

    added search

--- app/routes/demo.tsx
+++ app/routes/demo.tsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import { LoaderFunctionArgs, json } from '@remix-run/node'
+import { Form, useFetcher, useLoaderData, useSubmit } from '@remix-run/react'
+import { useState } from 'react'
+import { db } from '~/services/database.server'
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+	// get the query string
+	const url = new URL(request.url)
+	const q = (url.searchParams.get('q') || '').trim()
+
+	const products = await db.products
+		.find(
+			{
+				$or: [
+					{ name: { $regex: q } },
+					{ brand: { $regex: q } },
+					{ department: { $regex: q } }
+				]
+			},
+			{ limit: 10 }
+		)
+		.toArray()
+
+	return json({ products, q })
+}
 
 export default function Demo() {
+	const { products, q } = useLoaderData<typeof loader>()
+	const submit = useSubmit()
+	const fetcher = useFetcher()
+
 	return (
 		<div>
-			<input className='field' placeholder='hello'></input>
+			<fetcher.Form
+				onChange={e => {
+					submit(e.currentTarget, {
+						replace: q != null
+					})
+				}}>
+				<input name='q' type='search' defaultValue={q || ''} />
+				<input type='submit' />
+			</fetcher.Form>
+
+			<pre>{JSON.stringify(products, null, 2)}</pre>
 		</div>
 	)
 }

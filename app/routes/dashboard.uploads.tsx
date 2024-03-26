c0fd3b078ea3a99fc678f60609102dc4e8eac5ee
laszloekovacs <laszloekovacs@gmail.com>
2024-03-26 13:58:06 +0100

    added file upload

--- app/routes/dashboard.uploads.tsx
+++ app/routes/dashboard.uploads.tsx
@@ -7,7 +7,10 @@ import {
 	unstable_createMemoryUploadHandler,
 	unstable_parseMultipartFormData
 } from '@remix-run/node'
-import { Form } from '@remix-run/react'
+import { Form, Outlet, useFetcher, useLoaderData } from '@remix-run/react'
+import fs from 'fs'
+
+const max_size = 2_000_000
 
 export const action = async ({ request }: ActionFunctionArgs) => {
 	// get todays date part
@@ -16,7 +19,7 @@ export const action = async ({ request }: ActionFunctionArgs) => {
 
 	const uploadHandler = unstable_composeUploadHandlers(
 		unstable_createFileUploadHandler({
-			maxPartSize: 5_000_000,
+			maxPartSize: max_size,
 			file: ({ filename }) => filename,
 			directory: 'public/uploads/' + today
 		}),
@@ -29,17 +32,35 @@ export const action = async ({ request }: ActionFunctionArgs) => {
 	return null
 }
 
+export const loader = async () => {
+	// list all folders in public/uploads
+	const folders = await fs.promises.readdir('public/uploads', {
+		withFileTypes: true
+	})
+
+	const dirs = folders.filter(folder => folder.isDirectory())
+
+	const folderNames = dirs.map(dir => {
+		return dir.name
+	})
+
+	return json({ folderNames })
+}
+
 export default function UploadsPage() {
+	const fetcher = useFetcher()
+	const data = useLoaderData<typeof loader>()
+
 	return (
 		<section className='container'>
 			<div className='row'>
 				<div className='col-8 mx-auto'>
 					<h2 className='mb-3'>Uploads</h2>
 
-					<Form encType='multipart/form-data' method='POST'>
+					<fetcher.Form encType='multipart/form-data' method='POST'>
 						<div className='mb-3'>
 							<label className='form-label'>
-								Upload files, max size is ~ 5mb
+								Upload files, max size is ~ {max_size / 1_000_000} MB per file
 							</label>
 							<input
 								className='form-control'
@@ -48,10 +69,21 @@ export default function UploadsPage() {
 								multiple
 							/>
 						</div>
+
 						<button className='btn btn-primary' type='submit'>
 							upload
 						</button>
-					</Form>
+					</fetcher.Form>
+				</div>
+			</div>
+			<div className='row'>
+				<div className='col-8 mx-auto'>
+					<h2 className='mb-3'>Uploaded Folders</h2>
+					<ul>
+						{data.folderNames.map((folder: string) => (
+							<li key={folder}>{folder}</li>
+						))}
+					</ul>
 				</div>
 			</div>
 		</section>

d02d2bd1eba9519154cf7ed53c589874b7c61b56
Laszlo E Kovacs <laszloekovacs@gmail.com>
2024-03-28 16:41:50 +0100

    updated

--- remix.config.js
+++ remix.config.js
@@ -1,8 +1,8 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
 	ignoredRouteFiles: ['**/*.css']
-	// appDirectory: "app",
-	// assetsBuildDirectory: "public/build",
-	// publicPath: 'public/'
+	//appDirectory: 'app',
+	//assetsBuildDirectory: '/public/build',
+	//publicPath: '/public/'
 	// serverBuildPath: "build/index.js",
 }

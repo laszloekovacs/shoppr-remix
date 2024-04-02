3e2e5b813f4ddc32f6752289c0549970681adf2a
laszloekovacs <laszloekovacs@gmail.com>
2024-04-02 12:09:09 +0200

    added vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,10 @@ import { vitePlugin as remix } from '@remix-run/dev'
 import { installGlobals } from '@remix-run/node'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import dotenv from 'dotenv'
 
 installGlobals()
+dotenv.config()
 
 export default defineConfig({
 	plugins: [remix(), tsconfigPaths()],
